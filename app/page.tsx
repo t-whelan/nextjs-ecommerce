40664ecc4c56154b3585e445de868eb184a641e1
t-whelan <whelan.tionne@gmail.com>
2024-07-22 14:41:17 +0200

    configure shadcn and font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
-
+import "@/styles/globals.css";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
       <h1>Home Page !!!</h1>
+      <Button>Click to Sign in</Button>
     </main>
   );
 }
